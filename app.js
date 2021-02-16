413b57f29ff5218727f4cb8bb4af85ebbee865ac
CarolinaFreitas <carolinafreits@gmail.com>
2021-02-16 14:31:26 -0300

    rascunho html e js; cria arquivos

---
+++ app.js
@@ -0,0 +1,32 @@
+// ao clicar em um botão enviar valor para a função rodada()
+
+
+//função computerplay()
+//  retorna aleatoriamente Rock Paper ou Scissors
+
+//função rodada(playerSelection, computerSelection)
+//    variavel computerSelection = função computerPlay()
+//    variavel playerSelection = entrada do botão clicado   
+//    compara os valores
+//    aumenta os contadores de placar
+//    cria item no histórico
+//    retorna string que declara o resultado "You [win/lose]! {itemDominante} beats {itemDominado}"
+
+
+
+
+
+
+//função rodada (playerSelection, computerSelection)
+//  se receber texto a função deve ser insensível a caixa da letra
+// retorna string que declara "You [win/lose]! {itemDominante} beats {itemDominado}"
+
+
+//função game()
+//  variaveis contadores de placar
+//  variavel contador de rodadas
+//  guarda os resultados de 5 rodadas
+//  se contador de rodadas =5 
+//      calcula o vencedor
+//      retorna o resultado
+
